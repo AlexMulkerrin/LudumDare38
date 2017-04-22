0f4f45f16eda32824b7090e5025dbb3815f4dfbf
AlexMulkerrin <a.mulkerrin.12@aberdeen.ac.uk>
2017-04-22 07:42:12 +0100

    First code commit :P

---
+++ JavaScript/Program/Program.js
@@ -0,0 +1,10 @@
+// global reference to program object
+var asteroidTerrarium;
+
+function loadProgram() {
+	asteroidTerrarium = new Program();
+}
+
+function Program() {
+	console.log("Hello Ludum Dare #38!");
+}
